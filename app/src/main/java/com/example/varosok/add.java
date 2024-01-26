78d64ac4dc746f03e375dfe10d36198d29aee762
Klibit <kovacsbence720@gmail.com>
2024-01-26 22:04:27 +0100

    Add page xml

--- app/src/main/java/com/example/varosok/add.java
+++ app/src/main/java/com/example/varosok/add.java
@@ -9,6 +9,6 @@ public class add extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add);
+        setContentView(R.layout.activity_add2);
     }
 }
