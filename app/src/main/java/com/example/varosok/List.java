44c0fbee07bbcb6807118243d260d274b0a5b7e1
Klibit <kovacsbence720@gmail.com>
2024-01-27 21:09:35 +0100

    List page java file

--- app/src/main/java/com/example/varosok/List.java
+++ app/src/main/java/com/example/varosok/List.java
@@ -3,12 +3,32 @@ package com.example.varosok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class List extends AppCompatActivity {
+    private Button backButtonListPage;
+    private ListView cityListview;
+    private EditText editTextID;
+    private List<Cities> cities = new ArrayList<>();
+    private String url = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        init() ;
     }
+    public void init(){
+        backButtonListPage = findViewById(R.id.backButtonListPage);
+        cityListview = findViewById(R.id.listViewListPage);
+        editTextID = findViewById(R.id.editTextId);
+
+
+
+    }
+
 }
--- app/src/main/java/com/example/varosok/add.java
+++ app/src/main/java/com/example/varosok/add.java
@@ -3,8 +3,16 @@ package com.example.varosok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class add extends AppCompatActivity {
+    private Button addButtonAddPage;
+    private Button backButtonBackPage;
+    private EditText nameTextAddPage;
+    private EditText countryTextAddPage;
+    private EditText populationTextAddPage;
+    private EditText editTextIdAddPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
